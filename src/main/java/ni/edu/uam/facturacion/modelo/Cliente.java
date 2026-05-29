a9aa0fcee21e8d3b7f582c92e1c0a948d37e6ebe
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-29 15:45:35 -0600

    feat: agregar clase models.Cliente

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
@@ -0,0 +1,22 @@
+package ni.edu.uam.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity @Getter
+@Setter
+public class Cliente {
+
+    @Id
+    @Column(length=6)
+    int numero;
+    @Column(length = 50)
+    @Required
+    String nombre;
+
+}
