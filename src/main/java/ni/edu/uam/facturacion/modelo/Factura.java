2c2c8f0585f3a3055b35947e0dd19d09506d8554
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-29 16:03:57 -0600

    feat: agregar clase modelo.Factura

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
@@ -0,0 +1,32 @@
+package ni.edu.uam.facturacion.modelo;
+import java.time.*;
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+import lombok.*;
+@Entity @Getter @Setter
+public class Factura {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @Hidden
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=4)
+    @DefaultValueCalculator(CurrentYearCalculator.class) // Año actual
+    int anyo;
+
+    @Column(length=6)
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+}
