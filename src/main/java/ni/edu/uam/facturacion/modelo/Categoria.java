de2de2d55abf1c89dd293f08bb4d0104119f7dee
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-29 15:48:30 -0600

    feat: agregar clase modelo.Categoria

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Categoria.java
@@ -0,0 +1,28 @@
+package ni.edu.uam.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Categoria {
+
+    @Id
+    @Hidden // La propiedad no se muestra al usuario. Es un identificador interno
+    @GeneratedValue(generator="system-uuid") // Identificador Universal Único (1)
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=50)
+    String descripcion;
+
+}
