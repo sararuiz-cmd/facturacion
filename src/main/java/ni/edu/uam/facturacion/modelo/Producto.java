2eca0e444e6ce318e9383aca04acb8e3196ff297
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-29 15:55:39 -0600

    feat: agregar funcionalidades nuevas a modelo.Producto

--- src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
@@ -2,11 +2,15 @@ package ni.edu.uam.facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.openxava.annotations.Files;
+import org.openxava.annotations.Money;
 import org.openxava.annotations.Required;
+import org.openxava.annotations.TextArea;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @Entity
 @Getter
@@ -18,5 +22,15 @@ public class Producto {
 
     @Column(length=50) @Required
     String descripcion;
+
+    @Money // La propiedad precio se usa para almacenar dinero
+    BigDecimal precio; // BigDecimal se suele usar para dinero
+
+    @Files // Una galería de fotos completa está disponible
+    @Column(length=32) // La cadena de 32 de longitud es para almacenar la clave de la galería
+    String fotos;
+
+    @TextArea // Esto es para un texto grande, se usará un área de texto o equivalente
+    String observaciones;
 
 }
