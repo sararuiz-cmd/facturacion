b934f8f195176ab1430aa5251a5fb0f4a56a7548
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-29 16:14:31 -0600

    feat: agregar calculador a factura

--- src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
@@ -1,6 +1,8 @@
 package ni.edu.uam.facturacion.modelo;
 import java.time.*;
 import javax.persistence.*;
+
+import ni.edu.uam.facturacion.calculadores.CalculadorSiguienteNumeroParaAnyo;
 import org.hibernate.annotations.GenericGenerator;
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
@@ -28,5 +30,12 @@ public class Factura {
 
     @TextArea
     String observaciones;
+    @Column(length=6)
+    @DefaultValueCalculator(value= CalculadorSiguienteNumeroParaAnyo.class,
+            properties=@PropertyValue(name="anyo") // Para inyectar el valor de anyo de Factura
+            // en el calculador antes de llamar a calculate()
+    )
+    int numero_cal;
+
 
 }
