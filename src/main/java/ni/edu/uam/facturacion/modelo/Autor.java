7ac5c3d2d8041ba4815af4039570bbbbc6552373
Sara Ruiz <svruiz@uamv.edu.ni>
2026-05-31 22:59:27 -0600

    feat: agregar clase Autor

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Autor.java
@@ -0,0 +1,17 @@
+package ni.edu.uam.facturacion.modelo;
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import lombok.*;
+@Entity @Getter @Setter
+public class Autor {
+
+    @Id @GeneratedValue(generator="system-uuid") @Hidden
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=50) @Required
+    String nombre;
+
+}
